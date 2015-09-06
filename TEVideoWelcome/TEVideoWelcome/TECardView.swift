1815d78fa89b3a30537b81678733540f4e347346
ytdxxt10 <ytdxxt10@163.com>
2015-09-06 16:14:05 +0800

    s

---
+++ TEVideoWelcome/TEVideoWelcome/TECardView.swift
@@ -0,0 +1,70 @@
+//
+//  TECardView.swift
+//  TEVideoWelcome
+//
+//  Created by offcn on 15/9/6.
+//  Copyright (c) 2015年 Terry. All rights reserved.
+//
+
+import UIKit
+
+class CardView: UIView {
+    var userName:UITextField?
+    var passWord:UITextField?
+    var judge:Bool?
+    init() {
+        var viewFrame = CGRectMake(0, 0, UIScreen.mainScreen().bounds.width, UIScreen.mainScreen().bounds.height*2/3)
+        super.init(frame: viewFrame)
+        self.backgroundColor = UIColor.clearColor()
+        self.layer.cornerRadius = 10.0
+        self.clipsToBounds = true
+        
+        for index in 0...1{
+            var filed = UITextField(frame: CGRectMake(0, 0, 220, 30))
+            filed.borderStyle = UITextBorderStyle.RoundedRect
+            filed.center = CGPointMake(self.frame.size.width/2, CGFloat(200+index*50))
+            if index==0 {
+                judge = true
+                
+            }else{
+                judge = false
+            }
+            filed.placeholder = judge! ? "passWord":"userName"
+            filed.secureTextEntry = judge!
+            filed.keyboardAppearance = UIKeyboardAppearance.Dark
+            if(!judge!) {
+                
+                self.userName = filed
+            }else {
+                
+                self.passWord = filed
+            }
+            self.addSubview(filed)
+            
+        }
+        
+    }
+    
+    required init(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func touchesBegan(touches: Set<NSObject>, withEvent event: UIEvent) {
+        for  subview in self.subviews {
+            subview.resignFirstResponder()
+            
+        }
+    }
+    
+    
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+    // Drawing code
+    }
+    */
+    
+    
+}
+
---
+++ TEVideoWelcome/TEVideoWelcome/TEViewController.swift
@@ -0,0 +1,254 @@
+//
+//  TEViewController.swift
+//  TEVideoWelcome
+//
+//  Created by offcn on 15/9/6.
+//  Copyright (c) 2015年 Terry. All rights reserved.
+//
+import UIKit
+import AVFoundation
+
+class ViewController: UIViewController {
+    enum currentStatus : Int {
+        case freeStatus
+        case loginStatus
+        case signupStatus
+    }
+    enum buttonDirection {
+        
+        case buttonLeft
+        case buttonRight
+    }
+    let leftButtonFreeTitle = "Login in"
+    let leftButtonLoginTItle = "Confirm Login"
+    let leftButtonSignUpTitle = "Confirm Sign up"
+    
+    let rightButtonFreeTitle  = "Sign up"
+    let rightButtonLoginTitle = "Cancel Login"
+    let rightButtonSignupTitle = "Cancel Signup"
+    
+    let leftButtonFreeAction = "loginClick"
+    let leftButtonLoginAction = "confirmClick"
+    let leftButtonSignupAction = "confirmClick"
+    
+    let rightButtonFreeAction = "signupClick"
+    let rightButtonLoginAction = "cancelClick"
+    let rightButtonSignupAction = "cancelClick"
+    
+    let button_padding = 20.0
+    var player:AVPlayer?
+    var leftButton:UIButton!
+    var rightButton:UIButton!
+    var status:currentStatus!
+    var titleLabel:UILabel!
+    var cardView:CardView = CardView()
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        self.status = currentStatus.freeStatus
+        
+        createVideoPlayer()
+        createTitleLabel()
+        createButton()
+        createShowAnimation()
+        
+        addCardView()
+        //        self.view.addSubview(cardView)
+        //        createButton()
+    }
+    
+    func createTitleLabel() {
+        
+        self.titleLabel = UILabel(frame: CGRectMake(0, 0, self.view.frame.size.width-80, 80))
+        self.titleLabel.alpha = 0.0
+        self.titleLabel.center = self.view.center
+        self.titleLabel.textAlignment = .Center
+        self.titleLabel.backgroundColor = UIColor.clearColor()
+        self.titleLabel.text = "UBER"
+        self.titleLabel.textColor = UIColor(white: 1.0, alpha: 0.2)
+        self.titleLabel.font = UIFont.systemFontOfSize(72)
+        self.view.addSubview(self.titleLabel)
+        
+        
+        
+    }
+    
+    func createShowAnimation () {
+        var animation : CABasicAnimation = CABasicAnimation(keyPath: "opacity")
+        animation.fromValue = 0.0
+        animation.toValue = 1.0
+        animation.duration = 4.0
+        self.titleLabel.layer.addAnimation(animation, forKey: "alpha")
+        
+        var keyAnimation = CAKeyframeAnimation(keyPath: "opacity")
+        keyAnimation.duration = 4.0
+        keyAnimation.values = [0.0,1.0,0.0]
+        keyAnimation.keyTimes = [0.0,0.35,1.0]
+        self.titleLabel.layer.addAnimation(keyAnimation, forKey: "opacity")
+        
+    }
+    
+    func createVideoPlayer( ){
+        var path = NSBundle.mainBundle().pathForResource("welcome_video", ofType: "mp4")
+        var url = NSURL(fileURLWithPath: path!)
+        var playerItem : AVPlayerItem = AVPlayerItem(URL: url)
+        playerItem.addObserver(self, forKeyPath: "status", options: NSKeyValueObservingOptions.New, context: nil)
+        
+        self.player = AVPlayer.playerWithPlayerItem(playerItem) as? AVPlayer
+        self.player?.volume = 0.0
+        var playerLayer = AVPlayerLayer(player: self.player)
+        playerLayer.videoGravity = "\(UIViewContentMode.ScaleToFill)"
+        playerLayer.frame = self.view.bounds
+        self.view.layer.addSublayer(playerLayer)
+        self.player?.play()
+        self.player?.currentItem.addObserver(self, forKeyPath: AVPlayerItemDidPlayToEndTimeNotification, options: NSKeyValueObservingOptions.New, context: nil)
+        
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: Selector("moviePlayDidEnd:"), name: AVPlayerItemDidPlayToEndTimeNotification, object: self.player?.currentItem)
+        
+    }
+    override func observeValueForKeyPath(keyPath: String, ofObject object: AnyObject, change: [NSObject : AnyObject], context: UnsafeMutablePointer<Void>) {
+        
+    }
+    //保证视频循环播放
+    func moviePlayDidEnd(notification:NSNotification) {
+        var playerItem:AVPlayerItem = notification.object as! AVPlayerItem
+        player?.seekToTime(kCMTimeZero)
+        self.player?.play()
+        
+    }
+    
+    func createButton() {
+        self.leftButton = self.createButtonWithTitleAndIndexAndAction("Login in", index: buttonDirection.buttonLeft, action: Selector("loginClick"))
+        self.view.addSubview(self.leftButton)
+        self.rightButton = self.createButtonWithTitleAndIndexAndAction("Register in", index: buttonDirection.buttonRight, action: Selector("signupClick"))
+        self.view.addSubview(self.rightButton)
+        
+    }
+    func createButtonWithTitleAndIndexAndAction(title:String,index:buttonDirection,action:Selector)->UIButton {
+        
+        var windth:CGFloat = CGFloat((Int(self.view.frame.size.width) - Int(3*button_padding))/2)
+        var button = UIButton(frame: CGRectMake(0, 0, windth, 30))
+        var x:Int = Int(self.view.frame.size.width / 4)
+        var y = index.hashValue * Int(self.view.frame.size.width / 2)
+        var xWindth:CGFloat = CGFloat(x + y)
+        button.center = CGPointMake(xWindth, CGFloat(self.view.frame.size.height-30))
+        button.addTarget(self, action: action, forControlEvents: .TouchUpInside)
+        button.setTitle(title, forState: .Normal)
+        button.layer.borderColor = UIColor.whiteColor().CGColor
+        button.layer.borderWidth = 1
+        button.tag = 1000+index.hashValue
+        button.layer.cornerRadius = 10
+        button.layer.masksToBounds = true
+        button.setTitleColor(UIColor.whiteColor(), forState: .Normal)
+        
+        return button
+    }
+    
+    //点击事件
+    func loginClick() {
+        
+        self.transitionToNewStatus(currentStatus.loginStatus)
+        
+    }
+    func signupClick() {
+        
+        self.transitionToNewStatus(currentStatus.signupStatus)
+        
+    }
+    func confirmClick() {
+        
+        for subview in self.cardView.subviews {
+            
+            subview.resignFirstResponder()
+            
+        }
+        self.transitionToNewStatus(currentStatus.freeStatus)
+        
+    }
+    func cancelClick() {
+        
+        for subview in self.cardView.subviews {
+            
+            subview.resignFirstResponder()
+            
+        }
+        self.transitionToNewStatus(currentStatus.freeStatus)
+        
+    }
+    // 当前按钮状态发生改变时改变按钮标题和事件
+    
+    func transitionToNewStatus(newStatus:currentStatus) {
+        
+        var leftButtonTitles = [leftButtonFreeTitle, leftButtonLoginTItle, leftButtonSignUpTitle];
+        var rightButtonTitles = [rightButtonFreeTitle, rightButtonLoginTitle, rightButtonSignupTitle];
+        var leftButtonActions = [leftButtonFreeAction, leftButtonLoginAction, leftButtonSignupAction];
+        var rightButtonActions = [rightButtonFreeAction, rightButtonLoginAction, rightButtonSignupAction];
+        
+        //移除按钮事件
+        self.leftButton.removeTarget(self, action: NSSelectorFromString(leftButtonActions[self.status.hashValue]), forControlEvents: UIControlEvents.TouchUpInside)
+        self.rightButton.removeTarget(self, action: NSSelectorFromString(rightButtonActions[self.status.hashValue]), forControlEvents: .TouchUpInside)
+        self.status = newStatus
+        
+        //刷新按钮事件
+        self.leftButton.addTarget(self, action: NSSelectorFromString(leftButtonActions[newStatus.hashValue]), forControlEvents: .TouchUpInside)
+        self.leftButton.setTitle(leftButtonTitles[newStatus.hashValue], forState: .Normal)
+        
+        self.rightButton.addTarget(self, action: NSSelectorFromString(rightButtonActions[newStatus.hashValue]), forControlEvents: .TouchUpInside)
+        self.rightButton.setTitle(rightButtonTitles[newStatus.hashValue], forState: .Normal)
+        
+        switch (self.status.hashValue) {
+            
+        case currentStatus.freeStatus.hashValue :
+            hideCardView()
+        case currentStatus.loginStatus.hashValue:
+            showCardView()
+        case currentStatus.signupStatus.hashValue:
+            showCardView()
+        default :
+            print("haha")
+            
+        }
+        
+        
+        
+        
+    }
+    func addCardView() {
+        
+        self.cardView.center = CGPointMake(CGRectGetMidX(self.view.bounds), -CGRectGetMidY(self.cardView.bounds));
+        self.view.addSubview(cardView)
+    }
+    func hideCardView () {
+        
+        self.cardView.userName?.text  = ""
+        self.cardView.passWord?.text = ""
+        UIView.animateWithDuration(0.5, animations: {
+            
+            self.cardView.center = CGPointMake(self.cardView.center.x, self.cardView.center.y-500)
+            
+        })
+    }
+    
+    func showCardView() {
+        self.cardView.userName?.text  = ""
+        self.cardView.passWord?.text = ""
+        UIView.animateWithDuration(0.5, animations: {
+            
+            self.cardView.center = CGPointMake(self.cardView.center.x, self.cardView.center.y+500)
+            
+        })
+        
+        
+        
+    }
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
+
+
